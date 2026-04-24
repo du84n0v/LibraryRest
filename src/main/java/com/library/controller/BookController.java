1080250a10e768ed5afb04adedeab9967d73e9af
dusanov <sadriddindusanov@gmail.com>
2026-04-24 12:15:59 +0500

    Put and Delete left for Book

--- src/main/java/com/library/controller/BookController.java
+++ src/main/java/com/library/controller/BookController.java
@@ -1,4 +1,39 @@
 package com.library.controller;
 
+import com.library.dto.BookDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("book")
 public class BookController {
+    List<BookDto> books = new ArrayList<>();
+
+    @GetMapping("/list")
+    public List<BookDto> getBookList(){
+        return books;
+    }
+
+    @PostMapping("create")
+    public BookDto createBook(@RequestBody BookDto dto){
+        if(dto.getAuthor().isEmpty() || dto.getTitle().isEmpty()) return null;
+        dto.setId(UUID.randomUUID().toString());
+        dto.setPublishedYear(LocalDateTime.now());
+
+        books.add(dto);
+        return dto;
+    }
+
+    @GetMapping("/by-id/{id}")
+    public BookDto getBookById(@PathVariable String id){
+        for(BookDto bookDto :books){
+            if (bookDto.getId().equals(id)) return bookDto;
+        }
+        return null;
+    }
+
 }
--- src/main/java/com/library/controller/StudentBookController.java
+++ src/main/java/com/library/controller/StudentBookController.java
@@ -1,4 +1,10 @@
 package com.library.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("student-book")
 public class StudentBookController {
 }
